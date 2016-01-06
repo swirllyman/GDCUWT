9e3604b659c4fd9769defc931dfc202eb85bbc7a
Joe Greive <swirllyman@gmail.com>
2016-01-06 13:21:15 -0800

    added object manager

---
+++ Assets/Scripts/ObjectiveManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectiveManager : MonoBehaviour {
+
+    public GameObject[] objectives;
+    public GameObject currentClosestObject;
+    public GameObject targetItem;
+    // Use this for initialization
+    void Start () {
+        objectives = GameObject.FindGameObjectsWithTag("Interact");
+	}
+
+    void Update()
+    {
+        if(currentClosestObject != null)
+            targetItem.transform.LookAt(currentClosestObject.transform.position);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    foreach(GameObject go in objectives)
+        {
+            if(currentClosestObject == null)
+            {
+                currentClosestObject = go;
+            }
+            if(Vector3.Distance(transform.position, go.transform.position) < Vector3.Distance(transform.position, currentClosestObject.transform.position))
+            {
+                currentClosestObject = go;
+            }
+        }
+	}
+}
