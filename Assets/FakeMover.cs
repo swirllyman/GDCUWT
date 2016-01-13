038876a57a49309b3fb725892ad68bcd6b8650e8
Joe Greive <swirllyman@gmail.com>
2016-01-13 13:50:03 -0800

    Added the Fake mover script (Was for testing, will eventuall become our glider)

---
+++ Assets/FakeMover.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FakeMover : MonoBehaviour {
+    public float rotationOffset = 5;
+    public float flyAmount = 25;
+    Rigidbody body;
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody>();
+	}
+	
+    void FixedUpdate()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationOffset, 0);
+        body.AddForce(transform.forward * Input.GetAxisRaw("Vertical") * 50);
+
+        if (Input.GetButton("Jump"))
+        {
+            body.AddForce(0, flyAmount, 0);
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+    
+}
