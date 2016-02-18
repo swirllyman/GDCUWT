1a814e84bb5d83976148db65654d9ba1d6183317
Joe Greive <swirllyman@gmail.com>
2016-02-17 19:38:25 -0800

    A bunch of crafting changes from today
    Includes:
    Outline shader manipulation
    Reorganization of craft stuff

---
+++ Assets/CraftableItem.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CraftableItem : MonoBehaviour {
+
+    bool hovering = false;
+    bool selected = false;
+
+
+    Vector3 currentPoint;
+    Vector3 startingPos;
+
+    public GameObject realObject;
+
+    bool created = false;
+    // Use this for initialization
+    void Start () {
+        gameObject.layer = 2;
+        startingPos = transform.position;
+	}
+
+    void Select(bool select)
+    {
+        if (select)
+        {
+            selected = true;
+            gameObject.layer = 2;
+        }
+        else
+        {
+            selected = false;
+            gameObject.layer = 0;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (realObject == null &! created) {
+            gameObject.layer = 0;
+            GetComponent<Renderer>().enabled = true;
+            created = true;
+        }
+
+        if (hovering)
+        {
+            float currentOutlineAmount = GetComponent<Renderer>().material.GetFloat("_Outline");
+            float lerpValue = Mathf.Lerp(currentOutlineAmount, .015f, Time.deltaTime * 20);
+            GetComponent<Renderer>().material.SetFloat("_Outline", lerpValue);
+            GetComponent<ParticleSystem>().enableEmission = true;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Select(true);
+            }
+        }
+        else
+        {
+            float currentOutlineAmount = GetComponent<Renderer>().material.GetFloat("_Outline");
+            float lerpValue = Mathf.Lerp(currentOutlineAmount, 0, Time.deltaTime * 20);
+            GetComponent<Renderer>().material.SetFloat("_Outline", lerpValue);
+            GetComponent<ParticleSystem>().enableEmission = false;
+        }
+
+
+        if (selected)
+        {
+            currentPoint = Input.mousePosition;
+            currentPoint = Camera.main.ScreenToWorldPoint(currentPoint);
+            currentPoint.y = 458f;
+            transform.position = currentPoint;
+            transform.localPosition = new Vector3(transform.localPosition.x - .005f, transform.localPosition.y, transform.localPosition.z);
+            if (Input.GetMouseButtonUp(0))
+            {
+                Select(false);
+            }
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag != "CraftTable2")
+                {
+                    Debug.Log("Out of range, You hit: " + hit.collider.tag);
+                    Select(false);
+                    transform.position = startingPos;
+                }
+            }
+
+        }
+
+
+	}
+
+    void OnMouseEnter()
+    {
+        hovering = true;
+    }
+
+    void OnMouseExit()
+    {
+        hovering = false;
+    }
+}
--- Assets/Scripts/CraftingItems.cs
+++ Assets/Scripts/CraftingItems.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class CraftingItems : MonoBehaviour {
 	public float speed = 1;
-	public Camera mainCamera;
-
-	public Camera craftCamera;
+    public Camera[] cams;
 	private bool crafting;
 	private GameObject currentCraftObject;
 	private GameObject currentObject;
@@ -21,10 +19,8 @@ public class CraftingItems : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<WalkAround>();
-        craftCamera = GameObject.FindGameObjectWithTag("Craft Cam").GetComponent<Camera>();
-        mainCamera.enabled = true;
-        craftCamera.enabled = false;
-        crafting = false;
+        cams[1] = GameObject.FindGameObjectWithTag("Craft Cam").GetComponent<Camera>();
+        cams[1].tag = "MainCamera";
 	}
 	
 	// Update is called once per frame
@@ -34,90 +30,25 @@ public class CraftingItems : MonoBehaviour {
 			{
 				switch(currentObject.tag){
 				case "Interact" :
-                        transform.root.GetComponent<ObjectiveManager>().recheckArea(currentObject);
+                    transform.root.GetComponent<ObjectiveManager>().recheckArea(currentObject);
 					Destroy (currentObject);
 					break;
 				case "CraftingTable":
-					crafting = true;
+                    SwitchCams(cams[0], cams[1]);   
                     playerScript.crafting = true;
                     playerScript.HideCursor(false);
 					break;
 				}
 			}
 		}
-		if(crafting) {
-			currentObject.GetComponent<ParticleSystem>().enableEmission = false;
-			mainCamera.enabled = false;
-			craftCamera.enabled = true;
-			Ray ray = craftCamera.ScreenPointToRay(Input.mousePosition);
-			currentPoint = Input.mousePosition;
-			currentPoint = craftCamera.ScreenToWorldPoint(currentPoint);
-			currentPoint.y = 458f;
-			//if(Physics.Raycast(transform.position, out hit, 10000.0f)) {
-			if(Physics.Raycast (ray, out hit, 10000.0f)) {
-                if (hit.collider.tag == "CraftInteract") {
-                    lightUpObject = hit.collider.gameObject;
-                    if (lightUpObject.GetComponent<Renderer>().enabled) {
-                        lightUpObject.GetComponent<ParticleSystem>().enableEmission = true;
-                        if (Input.GetMouseButtonDown(0)) {
-                            currentCraftObject = hit.collider.gameObject;
-                            //Debug.Log(Input.mousePosition);
-                        }
-                    }
-                } else if (hit.collider.tag == "Border") {
-                    if (lightUpObject != null) {
-                        lightUpObject.GetComponent<ParticleSystem>().enableEmission = false;
-                        lightUpObject = null;
-                    }
-                    if (currentCraftObject != null) {
-                        currentCraftObject = null;
-                    }
-                } else if (hit.collider.tag == "Ground") {
-
-                    if (lightUpObject != null)
-                    {
-                        lightUpObject.GetComponent<ParticleSystem>().enableEmission = false;
-                        lightUpObject = null;
-                    }
-                    if (currentCraftObject != null)
-                    {
-                        currentCraftObject = null;
-                    }
-
-
-                } else if (lightUpObject != null) {
-                    lightUpObject.GetComponent<ParticleSystem>().enableEmission = false;
-                    lightUpObject = null;
-                }
-			} else if(lightUpObject != null) {
-				lightUpObject.GetComponent<ParticleSystem>().enableEmission = false;
-				lightUpObject = null;
-			}
-
-			if(currentCraftObject != null) {
-				Vector3.Lerp(transform.position, currentPoint, speed * Time.deltaTime);
-				Debug.Log (hit.transform.position);
-				currentCraftObject.transform.position = currentPoint;
-				if(Physics.Raycast (ray, out hit, 10000.0f)) {
-					if(hit.collider.tag == "Snap") {
-						Debug.Log ("Snap");
-						currentCraftObject.transform.position = new Vector3(hit.transform.position.x, hit.transform.position.y+25.0f, hit.transform.position.z);
-						currentCraftObject = null;
-					}
-				}
-				if(Input.GetMouseButtonUp (0)) {
-					currentCraftObject = null;
-				}
-			}
-
-		} else {
-			mainCamera.enabled = true;
-			craftCamera.enabled = false;
-		}
-	}
+    }
 
     float currentObjectEmissionRate;
-    
+
+    void SwitchCams(Camera a, Camera b) {
+        b.enabled = true;
+        a.enabled = false;
+    }
 	void OnTriggerEnter(Collider c){
 		if(c.gameObject.tag == "Interact" || c.gameObject.tag == "CraftingTable") {
             currentObjectEmissionRate = c.gameObject.GetComponent<ParticleSystem>().emissionRate;
@@ -140,7 +71,7 @@ public class CraftingItems : MonoBehaviour {
 	void OnGUI() {
 		if(crafting){
 			if(GUI.Button (new Rect(Screen.width - 100, 25, 75, 50), "Exit")) {
-				crafting = false;
+                SwitchCams(cams[1], cams[0]);
                 playerScript.crafting = false;
                 playerScript.HideCursor(true);
             }
